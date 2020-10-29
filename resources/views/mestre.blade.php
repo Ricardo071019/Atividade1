7ef83fa007b613e269d06aabb177e7af159eb889
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-29 17:52:18 +0000

    Update mestre.blade.php

--- resources/views/mestre.blade.php
+++ resources/views/mestre.blade.php
@@ -8,8 +8,14 @@ RikAgency
 @section('conteudo')
 
 	
+<br>
+<p style="text-align: center;">
 
-<img src="logo.png">
+	<img src="img/logo.png" width="650px" height="600px">
+	<p style="text-align: center; font-size: 30px; color: #33ccff">Mais sobre voos baratos na RikAgency</p>
+		<p style="text-align: center; font-size: 30px; color: #003399">Os viajantes usam a RikAgency para encontrar voos baratos</p>
+		<h1 style="text-align: center; font-size: 25px;"> Passagens de todas as companhias num só lugar. Poupa mais, viaja mais. Transparência nos preços. Sem taxas ou comissões escondidas. Cancelamentos Flexíveis. Ofertas Viagens Nacionais. Poupa Tempo e Dinheiro. Recebe Alertas de Voos. Previsão de Preços.</h1>
+</p>
 
 
 
